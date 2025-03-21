c4186d9957c603a649936e2dc0bfeead03d772f4
unilock <unilock@fennet.rentals>
2025-03-21 16:37:53 -0400

    re-implement handler dimensions; update handler modName
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/chromatinei/compat/IMCHandler.java
+++ src/main/java/cc/unilock/chromatinei/compat/IMCHandler.java
@@ -5,19 +5,21 @@ import net.minecraft.nbt.NBTTagCompound;
 
 public class IMCHandler {
     public static void init() {
-        sendHandler("cc.unilock.chromatinei.compat.nei.handler.CastingTableHandler", "ChromatiCraft:chromaticraft_item_placer:5");
+        sendHandler("cc.unilock.chromatinei.compat.nei.handler.CastingTableHandler", "ChromatiCraft:chromaticraft_item_placer:5", 226, 149);
         sendCatalyst("cc.unilock.chromatinei.compat.nei.handler.CastingTableHandler", "ChromatiCraft:chromaticraft_item_placer:5");
-        sendHandler("cc.unilock.chromatinei.compat.nei.handler.PoolHandler", "ChromatiCraft:chromaticraft_item_bucket");
+        sendHandler("cc.unilock.chromatinei.compat.nei.handler.PoolHandler", "ChromatiCraft:chromaticraft_item_bucket", 150, 130);
         sendCatalyst("cc.unilock.chromatinei.compat.nei.handler.PoolHandler", "ChromatiCraft:chromaticraft_item_bucket");
     }
 
-    private static void sendHandler(String handler, String workstation) {
+    private static void sendHandler(String handler, String workstation, int height, int width) {
         NBTTagCompound nbt = new NBTTagCompound();
         nbt.setString("handler", handler);
-        nbt.setString("modName", "ChromatiCraft");
+        nbt.setString("modName", "ChromatiNEI");
         nbt.setString("modId", "ChromatiCraft");
         nbt.setBoolean("modRequired", true);
         nbt.setString("itemName", workstation);
+        nbt.setInteger("handlerHeight", height);
+        nbt.setInteger("handlerWidth", width);
         nbt.setInteger("maxRecipesPerPage", 1);
         FMLInterModComms.sendMessage("NotEnoughItems", "registerHandlerInfo", nbt);
     }
